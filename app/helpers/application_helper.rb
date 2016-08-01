16946dd78a97051d7b34a0fae3c482672328dec8
bruno78 <devbruno78@gmail.com>
2016-08-01 00:47:54 -0400

    Add Markdown for youtube and vimeo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,5 @@
 module ApplicationHelper
+  require 'markdown_renderer_with_special_links'
   class HTMLwithPygments < Redcarpet::Render::HTML
     def block_code(code, language)
       Pygments.highlight(code, lexer: language)
@@ -18,3 +19,13 @@ module ApplicationHelper
     Redcarpet::Markdown.new(renderer, options).render(content).html_safe
   end
 end
+
+=begin
+1st code:
+Redcarpet::Markdown.new(renderer, options).render(content).html_safe
+2nd code:
+Redcarpet::Markdown.new(MarkdownRendererWithSpecialLinks, renderer, options).render(content).html_safe
+3rd code:
+@markdown ||= Redcarpet::Markdown.new(MarkdownRendererWithSpecialLinks, autolink: true, space_after_headers: true, fenced_code_blocks: true)
+@markdown.render(content).html_safe
+=end
