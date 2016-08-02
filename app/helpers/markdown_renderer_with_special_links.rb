c4337a94546821e9eef4ffdcd43592ab6d3d9b71
bruno78 <devbruno78@gmail.com>
2016-08-02 15:22:03 -0400

    Added share post features

--- app/helpers/markdown_renderer_with_special_links.rb
+++ app/helpers/markdown_renderer_with_special_links.rb
@@ -3,6 +3,7 @@ class MarkdownRendererWithSpecialLinks < Redcarpet::Render::HTML
     case link_type
       when :url then url_link(link)
       when :email then email_link(link)
+      when :pyrenderer then renderer = HTMLwithPygments.new(hard_wrap: true, filter_html: true)
     end
   end
 
@@ -33,4 +34,5 @@ class MarkdownRendererWithSpecialLinks < Redcarpet::Render::HTML
   def email_link(email)
     "<a href=\"mailto:#{email}\">#{email}</a>"
   end
+
 end
