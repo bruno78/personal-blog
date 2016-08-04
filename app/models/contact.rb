cc643566aadf9b7a78e47ac817adbd7f6ccd0779
bruno78 <devbruno78@gmail.com>
2016-08-03 23:58:33 -0400

    Setup contacts page Mailform and Validate email for

---
+++ app/models/contact.rb
@@ -0,0 +1,14 @@
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validates => :email, :email_format => { :message => 'is not looking good' }
+  attribute :message,   :validate => true
+  attribute :nickname,  :captcha  => true
+
+  def headers
+    {
+      :subject => "Contact Form",
+      :to => "devbruno78@gmail.com",
+      :from => %("#{name} <#{email}>")
+    }
+  end
+end
