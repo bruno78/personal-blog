9c56b0d20c831af6cafc81bfcc09f3ea41fbcc13
bruno78 <devbruno78@gmail.com>
2016-08-05 18:16:07 -0400

    changing tag counts for heroku Posgres

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,8 @@ class Tag < ActiveRecord::Base
   has_many :posts, through: :taggings
 
   def self.counts
-    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    self.select("tags.name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    # self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
   end
-  
+
 end
