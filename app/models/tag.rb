66967bae87417e20b16ca42ada6b37ebd3bcad3f
bruno78 <devbruno78@gmail.com>
2016-08-05 18:27:05 -0400

    fixing for heroku Posgres 2

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ActiveRecord::Base
   has_many :posts, through: :taggings
 
   def self.counts
-    Tag.select("tags.name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("tags.name, taggings.tag_id")
     # self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
   end
 
