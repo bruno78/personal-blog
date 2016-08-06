abebb22d5ed18204845281702e1e2b9e76dd12fe
bruno78 <devbruno78@gmail.com>
2016-08-05 21:40:43 -0400

    fixed tag issue

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,7 +4,7 @@ class Tag < ActiveRecord::Base
 
   def self.counts
     self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("tags.name, taggings.tag_id")
-    # self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id")
+    # self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id") sqlite
   end
 
 end
