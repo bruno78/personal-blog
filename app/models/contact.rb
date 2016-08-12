9634cc18c33f62f4afbfef1955fbb718445be416
bruno78 <devbruno78@gmail.com>
2016-08-11 20:55:04 -0400

    fixing the email form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
     {
       :subject => "Contact Form",
       :to => "devbruno78@gmail.com",
-      :from => %("#{name} <#{email}>")
+      :from => %("#{name}" <#{email}>)
     }
   end
 end
